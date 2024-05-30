a3ac7b3e83ecbecec0cb47491439289cfcc0b636
xuan <zx786@vip.qq.com>
2024-05-30 10:32:21 +0800

    feat: uuid和username加上唯一索引

--- server/model/user_model.go
+++ server/model/user_model.go
@@ -4,8 +4,8 @@ import "github.com/google/uuid"
 
 type User struct {
 	Model
-	UUID     uuid.UUID `json:"uuid" gorm:"index;comment:用户UUID"`             // 用户UUID
-	Username string    `json:"username" gorm:"index;comment:用户名"`            // 用户登录名
+	UUID     uuid.UUID `json:"uuid" gorm:"index:,unique;comment:用户UUID"`     // 用户UUID
+	Username string    `json:"username" gorm:"index:,unique;comment:用户名"`    // 用户登录名
 	Password string    `json:"-"  gorm:"comment:密码"`                         // 用户登录密码
 	Nickname string    `json:"nickname" gorm:"default:ez_todo;comment:用户昵称"` // 用户昵称
 }
